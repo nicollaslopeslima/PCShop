7d90881f89c15b9498487d22b4de82d650a39065
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-03-05 20:24:59 -0300

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using PCShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PCShop.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Hardware> Hardwares { get; set; }
+    }
+}
---
+++ Models/Hardware.cs
@@ -0,0 +1,18 @@
+﻿namespace PCShop.Models
+{
+    public class Hardware
+    {
+        public int HardwareId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsHardwarePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoraId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
