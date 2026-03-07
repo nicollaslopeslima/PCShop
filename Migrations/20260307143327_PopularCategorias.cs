82c1ab6ab377b28839a59feb35dec7f2d87e8a2e
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-03-07 11:43:19 -0300

    Seed the Categories table with initial data.

---
+++ Migrations/20260307143327_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PCShop.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+                "VALUES('Graphics Card', 'A hardware component responsible for rendering images, videos, and graphics on a computer display.')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+                "VALUES('CPU', 'The main hardware component responsible for executing instructions and processing data in a computer.')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
