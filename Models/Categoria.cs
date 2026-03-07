60298db3be514e4619656b83ae9b1263955a717b
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-03-07 00:07:13 -0300

    Applying Data Annotations attributes to the ‘Hardware’ and ‘Category’ classes.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace PCShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PCShop.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage = "Maximum length: 100 characters.")]
+        [Required(ErrorMessage = "Category name is required.")]
+        [Display(Name = "Name")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "Maximum length: 200 characters.")]
+        [Required(ErrorMessage = "Enter a description for the category.")]
+        [Display(Name = "Description")]
         public string Descricao { get; set; }
 
         public List<Hardware> Hardwares { get; set; }
--- Models/Hardware.cs
+++ Models/Hardware.cs
@@ -1,15 +1,49 @@
-﻿namespace PCShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PCShop.Models
 {
+    [Table("Hardwares")]
     public class Hardware
     {
+        [Key]
         public int HardwareId { get; set; }
+
+        [Required(ErrorMessage = "Hardware name is required.")]
+        [Display(Name = "Hardware Name")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "Hardware name must be between 10 and 80 characters.")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Hardware description is required.")]
+        [Display(Name = "Hardware Description")]
+        [MinLength(20, ErrorMessage = "Hardware description must be at least 20 characters long.")]
+        [MaxLength(200, ErrorMessage = "Hardware description cannot exceed 200 characters.")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "The detailed description of the hardware must be provided")]
+        [Display(Name = "Detailed Hardware Description.")]
+        [MinLength(20, ErrorMessage = "Hardware description must be at least 20 characters long.")]
+        [MaxLength(200, ErrorMessage = "Hardware description cannot exceed 200 characters.")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Hardware price is required.")]
+        [Display(Name = "Price")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 9999999.99, ErrorMessage = "The price must be between 1 and 9999999.99.")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Image URL")]
+        [StringLength(200, ErrorMessage = "Maximum length: 200 characters.")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Thumbnail Image URL")]
+        [StringLength(200, ErrorMessage = "Maximum length: 200 characters.")]
         public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Is Preferred Hardware?")]
         public bool IsHardwarePreferido { get; set; }
+
+        [Display(Name = "Stock")]
         public bool EmEstoque { get; set; }
 
         public int CategoraId { get; set; }
