78949599a254980a4a87e857ead2e154fe7d87db
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-03-12 22:04:43 -0300

    feat: Creating the Controller and View to display hardware.

---
+++ Controllers/HardwareController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCShop.Models;
+using PCShop.Repositories.Interfaces;
+
+namespace PCShop.Controllers
+{
+    public class HardwareController : Controller
+    {
+        private readonly IHardwareRepository _hardwareRepository;
+
+        public HardwareController(IHardwareRepository hardwareRepository)
+        {
+            _hardwareRepository = hardwareRepository;
+        }
+
+        public IActionResult List()
+        {
+            var hardware = _hardwareRepository.Hardwares;
+            return View(hardware);
+        }
+    }
+}
