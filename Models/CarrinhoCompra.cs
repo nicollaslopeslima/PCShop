fff4c71dc82c8bd683ae2673479eae899a21f342
nicollaslopeslima <nicollaslopeslima21@gmail.com>
2026-03-17 20:50:33 -0300

    feat: create shopping cart.

---
+++ Models/CarrinhoCompra.cs
@@ -0,0 +1,39 @@
+﻿using PCShop.Context;
+
+namespace PCShop.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sessão
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //atribui o Id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
